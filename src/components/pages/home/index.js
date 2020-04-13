172f380c899eb323972bc2719f5e277f22c72602
Thiago Nunes Batista <41683571+ThiagoNunesBatista@users.noreply.github.com>
2020-04-13 18:16:33 -0300

    Add home component

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './styles.css'
+import ImageBackground from '../../layout/imageBackground'
+import GitHubIcon from '@material-ui/icons/GitHub'
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <ImageBackground />
+      <div className="homeWrapper">
+        <h1 className="title">Welcome to Joke Center</h1>
+        <p className="subtitle">An Open Source Project That Uses Public APIs to Give You Some Funny.</p>
+        <a href="https://github.com/ThiagoNunesBatista/joke-center">
+          <GitHubIcon style = {{ fontSize: '40px' }}/>
+        </a>
+      </div>
+
     </div>
   )
 }
