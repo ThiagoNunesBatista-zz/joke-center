7b44eb46d5dc52dce41d754b30ea17dbd0b3beda
Thiago Nunes Batista <41683571+ThiagoNunesBatista@users.noreply.github.com>
2020-04-13 18:15:22 -0300

    Add background image component

---
+++ src/components/layout/imageBackground/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import './styles.css'
+
+const index = () => {
+  return (
+    <Fragment>
+      <div className="background"></div>
+      <div className="overlay"></div>
+    </Fragment>
+  )
+}
+
+export default index
