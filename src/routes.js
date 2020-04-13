2ef4bf81fd7b412d297bc0575a65e4e9335b77a5
Thiago Nunes Batista <41683571+ThiagoNunesBatista@users.noreply.github.com>
2020-04-13 18:14:47 -0300

    Add routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/pages/home'
+import ChuckNorris from './components/chuckNorris'
+import Navbar from './components/layout/navbar'
+import Footer from './components/layout/footer'
 
 const routes = () => {
   return (
     <Router>
+      <Navbar />
       <Switch>
-        <Route exact path="/" component={Home}></Route>
+        <div className="container">
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/chuck-norris" component={ChuckNorris} />
+        </div>
       </Switch>
+      <Footer />
     </Router>
   )
 }
