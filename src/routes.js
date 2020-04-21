ef91bfef9a49fa0f94230d66fc8f09f0e6093eff
Thiago Nunes Batista <41683571+ThiagoNunesBatista@users.noreply.github.com>
2020-04-21 18:48:03 -0300

    Add routes

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,33 @@
+// External Libraries
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './components/pages/home'
-import ChuckNorris from './components/chuckNorris'
-import Navbar from './components/layout/navbar'
-import Footer from './components/layout/footer'
+import styled from 'styled-components'
+
+// Internal Components
+import Home from './components/Home/Home'
+import ChuckNorrisFacts from './components/ChuckNorrisFacts/ChuckNorrisFacts'
+import Navbar from './components/Navbar/Navbar'
 
 const routes = () => {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <div className="container">
-          <Route exact path="/" component={Home}></Route>
-          <Route exact path="/chuck-norris" component={ChuckNorris} />
-        </div>
+        <Container>
+          <Route path='/' exact component={Home} />
+          <Route path='/chuck-norris' exact component={ChuckNorrisFacts} />
+        </Container>
       </Switch>
-      <Footer />
     </Router>
   )
 }
+
+const Container = styled.div`
+width: 100vw;
+height: 90vh;
+max-width: 1280px;
+margin: 0 auto;
+text-align: center;
+`
 
 export default routes
