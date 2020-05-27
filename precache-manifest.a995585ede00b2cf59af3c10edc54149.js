29b0cd0ce9f3e3da646bd164309d73af8cb5fc55
Thiago Nunes Batista <41683571+ThiagoNunesBatista@users.noreply.github.com>
2020-05-27 19:36:57 -0300

    Updates

---
+++ precache-manifest.a995585ede00b2cf59af3c10edc54149.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04b0503a295bf785492e1e8f01dd0e1",
+    "url": "/joke-center/index.html"
+  },
+  {
+    "revision": "d53bffa248488bc97733",
+    "url": "/joke-center/static/css/main.c791bc91.chunk.css"
+  },
+  {
+    "revision": "43c5ef0c1b1af2d861a4",
+    "url": "/joke-center/static/js/2.527f75a5.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/joke-center/static/js/2.527f75a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d53bffa248488bc97733",
+    "url": "/joke-center/static/js/main.13797f60.chunk.js"
+  },
+  {
+    "revision": "b566d0160c2eb3044ac6",
+    "url": "/joke-center/static/js/runtime-main.bda70086.js"
+  },
+  {
+    "revision": "629c47ce348a1f246b4705616efeb07c",
+    "url": "/joke-center/static/media/chuck-norris-icon.629c47ce.jpg"
+  },
+  {
+    "revision": "932157344d05bb6c7bc4a4470007f0f7",
+    "url": "/joke-center/static/media/dad-joke.93215734.jpeg"
+  }
+]);
